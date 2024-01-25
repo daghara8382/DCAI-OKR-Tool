800e07862cb7345be0e9349f21ac37b80a990a65
daghara8382 <114475485+daghara8382@users.noreply.github.com>
2024-01-25 15:11:16 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+include 'includes/config/_config.php';
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Login</title>
+</head>
+<body>
+	<span style='color:#0071c5;font-size:20px;'>Authentication in progress, please wait...</span>
+</body>
+
+</html>
